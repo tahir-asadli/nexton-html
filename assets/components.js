5c83f53c271d830fda4eef79af53826ec0baf746
Tahir Asadli <tahir-asadov@outlook.com>
2026-04-10 09:44:49 +0400

    Update Cart Drawer

--- assets/components.js
+++ assets/components.js
@@ -68,16 +68,21 @@ class CartButton extends HTMLElement {
   constructor() {
     super();
     this.cartCount = this.querySelector('[data-cart-count]');
+    this.cartButton = this.querySelector('[data-button]');
+
     this.updateCart = this.updateCart.bind(this);
+    this.toggleDrawer = this.toggleDrawer.bind(this);
   }
 
   connectedCallback() {
     this.updateCart(this, true)
     document.addEventListener(EVENTS.CART_UPDATED, this.updateCart)
+    this.cartButton.addEventListener('click', this.toggleDrawer)
   }
 
   disconnectedCallback() {
     document.removeEventListener(EVENTS.CART_UPDATED, this.updateCart);
+    this.cartButton.removeEventListener('click', this.toggleDrawer);
   }
 
   updateCart(self, initial = false) {
@@ -86,9 +91,53 @@ class CartButton extends HTMLElement {
       this.cartCount.classList.add('animate-cart');
       setTimeout(() => {
         this.cartCount.classList.remove('animate-cart');
+        document.dispatchEvent(new CustomEvent(EVENTS.OPEN_CART_DRAWER, {
+          bubbles: true
+        }));
       }, 500);
     }
+  }
+
+  toggleDrawer() {
+    document.dispatchEvent(new CustomEvent(document.documentElement.classList.contains('cart-drawer-open') ? EVENTS.CLOSE_CART_DRAWER : EVENTS.OPEN_CART_DRAWER, {
+      bubbles: true
+    }));
   }
 }
 
 customElements.define('cart-button', CartButton)
+
+class CartDrawer extends HTMLElement {
+  constructor() {
+    super();
+    this.closeButton = this.querySelector('[data-close]')
+    this.drawer = this.querySelector('[data-drawer]')
+    this.overlay = this.querySelector('[data-overlay]')
+
+    this.openDrawer = this.openDrawer.bind(this);
+    this.closeDrawer = this.closeDrawer.bind(this);
+  }
+
+  connectedCallback() {
+    document.addEventListener(EVENTS.OPEN_CART_DRAWER, this.openDrawer)
+    document.addEventListener(EVENTS.CLOSE_CART_DRAWER, this.closeDrawer)
+    this.closeButton.addEventListener('click', this.closeDrawer)
+    this.overlay.addEventListener('click', this.closeDrawer)
+  }
+
+  disconnectedCallback() {
+    document.removeEventListener(EVENTS.OPEN_CART_DRAWER, this.openDrawer);
+    document.removeEventListener(EVENTS.CLOSE_CART_DRAWER, this.closeDrawer);
+    this.closeButton.removeEventListener('click', this.closeDrawer)
+    this.overlay.removeEventListener('click', this.closeDrawer)
+  }
+
+  openDrawer() {
+    document.documentElement.classList.add('cart-drawer-open');
+  }
+  closeDrawer() {
+    document.documentElement.classList.remove('cart-drawer-open');
+  }
+}
+
+customElements.define('cart-drawer', CartDrawer)
